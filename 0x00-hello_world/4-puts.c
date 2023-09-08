b21a6200cd204b656052d841c4e80605ed20e5db
Ishmeal M Janneh <ishmealmjanneh849@gmail.com>
2023-09-07 17:57:09 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - A programme outputing lines using the puts function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
